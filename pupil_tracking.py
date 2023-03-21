20ea408a9d23357dafc3a466f5f73309e203e091
Sai Srinivas <mssrinu004@gmail.com>
2023-03-22 01:29:29 +0530

    Update pupil_tracking.py
    updated threshold and added cursor movement (only R,L)

--- pupil_tracking.py
+++ pupil_tracking.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import pyautogui
 
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -7,6 +8,7 @@ eye_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
+pupil = [(0, 0)]
 while True:
     ret, frame = cap.read()
     if ret is False:
@@ -36,16 +38,18 @@ while True:
                 break
             continue
         for (ex, ey, ew, eh) in eyes:
+            ey = ey+int(eh/4)
+            eh = int(eh/2)
             cv2.rectangle(roi_color, (ex, ey),
                           (ex + ew, ey + eh), (0, 255, 0), 2)
 
             frame1 = frame[faces[0][1]:faces[0][1]+faces[0]
                            [3], faces[0][0]:faces[0][0]+faces[0][2]]
 
-            eye_orig_image = frame1[eyes[0][1]:(
-                eyes[0][1]+eyes[0][3]), eyes[0][0]:(eyes[0][0]+eyes[0][2])]
+            eye_orig_image = frame1[eyes[0][1]+int(eyes[0][3]/4):(
+                eyes[0][1]+int(3*eyes[0][3]/4)), eyes[0][0]:(eyes[0][0]+eyes[0][2])]
 
-            cv2.imshow('iris', eye_orig_image)
+            # cv2.imshow('iris', eye_orig_image)
 
             roi = eye_orig_image
             rows, cols, _ = roi.shape
@@ -56,45 +60,43 @@ while True:
             value = 0
 
             _, threshold = cv2.threshold(
-                    gray_roi, 100, 255, cv2.THRESH_BINARY_INV)
+                gray_roi, 100, 255, cv2.THRESH_BINARY_INV)
             contour, _ = cv2.findContours(
-                    threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            area=0
+                threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            area = 0
             for cnt in contour:
-                    area = area+cv2.contourArea(cnt)
+                area = area+cv2.contourArea(cnt)
 
             while (value < 100):
 
                 _, threshold = cv2.threshold(
                     gray_roi, value, 255, cv2.THRESH_BINARY_INV)
                 th, tw = threshold.shape
+
                 split1 = threshold[0:int(th/2), 0:tw]
                 split2 = threshold[int(th/2):th, 0:tw]
+
                 contour1, _ = cv2.findContours(
                     split1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
                 contour2, _ = cv2.findContours(
                     split2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-                
-                area1=0
-                area2=0
+
+                area1 = 0
+                area2 = 0
                 for cnt in contour1:
                     area1 = area1+cv2.contourArea(cnt)
                 for cnt in contour2:
                     area2 = area2+cv2.contourArea(cnt)
-                
-                
-                if (area1 + area2>0.6*area):
-                    value=40
+
+                if (area1 + area2 > 0.6*area):
+                    value = 40
                     break
-                elif(area1>0.8*area2):
+                elif (area1 > 0.8*area2):
                     break
 
                 value = value+1
             print(value)
-            # value=40
-            # _, threshold = cv2.threshold(
-            #         gray_roi, value, 255, cv2.THRESH_BINARY_INV)
 
             contours, _ = cv2.findContours(
                 threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -102,23 +104,35 @@ while True:
                 contours, key=lambda x: cv2.contourArea(x), reverse=True)
             for cnt in contours:
                 (x, y, w, h) = cv2.boundingRect(cnt)
-                # cv2.drawContours(roi, [cnt], -1, (0, 0, 255), 3)
-                # cv2.rectangle(roi, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                # cv2.line(roi, (x + int(w/2), 0),
-                #          (x + int(w/2), rows), (0, 255, 0), 2)
-                # cv2.line(roi, (0, y + int(h/2)),
-                #          (cols, y + int(h/2)), (0, 255, 0), 2)
+
                 cv2.line(frame, (fx+ex+x + int(w/2), 0),
                          (fx+ex+x + int(w/2), row0), (0, 255, 0), 2)
                 cv2.line(frame, (0, fy+ey+y + int(h/2)),
                          (col0, fy+ey+y + int(h/2)), (0, 255, 0), 2)
+
+                pupil.append((fx+ex+x + int(w/2), fy+ey+y + int(h/2)))
+                print(ew,eh)
+                print(abs(pupil[len(pupil)-1][0]-pupil[len(pupil)-2][0]))
+
+                if ((x+int(w/2) < int(ew/2)) and (abs(pupil[len(pupil)-1][0]-pupil[len(pupil)-2][0]))>int(ew/7)):
+                    pyautogui.moveRel(10, 0, duration=0.01)
+                elif ((x+int(w/2) > int(2*ew/3)) and (abs(pupil[len(pupil)-1][0]-pupil[len(pupil)-2][0]))>int(ew/7)):
+                    pyautogui.moveRel(-10, 0, duration=0.01)
+                print(*pupil, sep=", ")
+
                 break
-            # cv2.imshow("Threshold", threshold)
+            cv2.imshow("Threshold", threshold)
             # cv2.imshow("gray roi", gray_roi)
             # cv2.imshow("Roi", roi)
             cv2.imshow('frame', frame)
 
+    if len(pupil) >= 3:
+        last = pupil[len(pupil)-1]
+        pupil = [(0, 0)]
+        pupil.append(last)
+
     key = cv2.waitKey(30)
     if key == 27:
         break
+
 cv2.destroyAllWindows()
