d0201475b29175ba3dcf3cf6a98edc0798849dfa
Sai Srinivas <94926227+Sai-004@users.noreply.github.com>
2023-04-29 01:25:59 +0530

    Rename pupil_tracking.py to pupil_tracking_2.py

--- pupil_tracking_2.py
+++ pupil_tracking_2.py
@@ -2,11 +2,13 @@ import cv2
 import numpy as np  
 import pyautogui 
 
+# Importing HaarCascades
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_eye.xml')
 
+# Start Video
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
@@ -16,7 +18,7 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    if (len(faces) == 0):
+    if (len(faces) == 0):                                               #moving to next frame if no face is being detected
         cv2.imshow('frame', frame)
         key = cv2.waitKey(30)
         if key == 27:
@@ -25,10 +27,10 @@ while True:
 
     for (fx, fy, fw, fh) in faces:
         # cv2.rectangle(frame, (fx, fy), (fx + fw, fy + fh), (255, 0, 0), 2)
-        roi_gray = gray[fy:fy+fh, fx:fx+int(fw/2)]
+        roi_gray = gray[fy:fy+int(fh/2), fx:fx+int(fw/2)]               # tracking only one eye (R) by sending half face
         roi_color = frame[fy:fy+fh, fx:fx+fw]
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
-        if (len(eyes) == 0):
+        if (len(eyes) == 0):                                            #moving to next frame if no eyes are detected
             cv2.imshow('frame', frame)
             key = cv2.waitKey(30)
             if key == 27:
@@ -36,24 +38,23 @@ while True:
             continue
             
         for (ex, ey, ew, eh) in eyes:
-            ey=ey+int(eh/4)
+            ey=ey+int(eh/4)                                             #removing eyebrows from roi
             eh=int(eh/2)            
             cv2.rectangle(roi_color, (ex, ey),
                           (ex + ew, ey + eh), (0, 255, 0), 2)
             frame1 = frame[faces[0][1]:faces[0][1]+faces[0]
                            [3], faces[0][0]:faces[0][0]+faces[0][2]]
 
-            eye_orig_image = frame1[eyes[0][1]+int(eyes[0][3]/4):(
+            roi = frame1[eyes[0][1]+int(eyes[0][3]/4):(
                 eyes[0][1]+int(3*eyes[0][3]/4)), eyes[0][0]:(eyes[0][0]+eyes[0][2])]
-            if eye_orig_image.shape[0]==0: 
+            
+            if roi.shape[0]==0: 
                 cv2.imshow('frame', frame)
                 key = cv2.waitKey(30)
                 if key == 27:
                     break
                 continue
-            # cv2.imshow('iris', eye_orig_image)
-    
-            roi = eye_orig_image
+            # cv2.imshow('iris', roi)
             rows, cols, _ = roi.shape
             row1, col1, _ = frame1.shape
             # if(len(roi)==0): break
@@ -61,98 +62,51 @@ while True:
             gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
             gray_roi = cv2.GaussianBlur(gray_roi, (7, 7), 0)
             # kernel=np.ones((5,5),np.uint8)
-            # gray_roi=cv2.dilate(gray_roi,kernel,iterations=2) 
-            # gray_roi=cv2.erode(gray_roi,kernel,iterations=2) 
+            # gray_roi=cv2.dilate(gray_roi,kernel,iterations=2)
+            # gray_roi=cv2.erode(gray_roi,kernel,iterations=2)
+            
             value = 10
-            t_area =roi.shape[0] * roi.shape[1]
-
-            
-            
-            # contour, _ = cv2.findContours(
-            #         threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+            t_area =roi.shape[0] * roi.shape[1]                     #total ared of eye frame
+            # Loop for the threshold value calculation
             while (value < 100):
 
                 _, threshold = cv2.threshold(
-                    gray_roi, value, 255, cv2.THRESH_BINARY_INV)
+                    gray_roi, value, 255, cv2.THRESH_BINARY_INV)    #thresholding the grayscale roi
                 
                 contours, _ = cv2.findContours(
-                threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  #finding contours in threshold
                 # area=0
+                
                 contours = sorted(
                     contours, key=lambda x: cv2.contourArea(x), reverse=True)
-                print(contours)
-                if(len(contours)>0):area=cv2.contourArea(contours[0])
+                # print(contours)
+                if(len(contours)>0):
+                    area=cv2.contourArea(contours[0])               #area of max contour
                 else:
                     value=value+1
                     continue
                 if(area==0):
                     value=value+1
                     continue
-                if(t_area/area<15):
+                if(t_area/area<15):                                 #if max contour area is 15th of eye frame, identifying it as pupil
                     break
                 value=value+1
-
-                    
-                    
                 
-                
-                # _, threshold = cv2.threshold(
-                #     gray_roi, value, 255, cv2.THRESH_BINARY_INV)
-                # th, tw = threshold.shape
-                # split1 = threshold[0:int(th/2), 0:tw]
-                # split2 = threshold[int(th/2):th, 0:tw]
-                # contour1, _ = cv2.findContours(
-                #     split1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-                # contour2, _ = cv2.findContours(
-                #     split2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-                
-                # area1=0
-                # area2=0
-                # for cnt in contour1:
-                #     area1 = area1+cv2.contourArea(cnt)
-                # for cnt in contour2:
-                #     area2 = area2+cv2.contourArea(cnt)
-                
-                
-                # if (area1 + area2>0.6*area):
-                #     value=40
-                #     break
-                # elif(area1>area2):
-                #     break
-
-                # value = value+1
-            # value=45
             print(value)
-            # threshold = cv2.adaptiveThreshold(
-                    # gray_roi, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C ,cv2.THRESH_BINARY_INV,11,2)
-            # _, threshold = cv2.threshold(
-            #         gray_roi, value, 255, cv2.THRESH_BINARY_INV)
-            
-            # contours, _ = cv2.findContours(
-            #     threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            
-            
-            # contours = sorted(
-            #     contours, key=lambda x: cv2.contourArea(x), reverse=True)
             
             (x_axis,y_axis),radius = cv2.minEnclosingCircle(contours[0])
-
             center = (int(x_axis),int(y_axis))
             radius = int(radius)
-
+            #pupil tracing by contour
             cv2.circle(roi,center,radius,(0,255,0),2)
-            # cv2.imshow("gray roi", gray_roi)
-            # cv2.imshow("Roi", roi)
             
             # area = cv2.contourArea(cnt)
             # if(area==0):continue
             b=t_area/area
             print (area,t_area,b)
-            cv2.putText(frame,f'{b}', (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, [255,0,0], 2)
-            cv2.imshow('Thresh',threshold)
-            cv2.imshow('frame', frame)
+            cv2.putText(frame,f'{b}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, [255,0,0], 2)
+            cv2.imshow('Threshold',threshold)
+            cv2.imshow('tracking_frame', frame)
 
 
     key = cv2.waitKey(30)
